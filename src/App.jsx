6a45ef80dcd292d9f89cc78224984e2d719dbcc6
rakib8680 <rakib.cst.1st@gmail.com>
2026-03-01 16:59:29 +0600

    Project Initialized

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">This is App component</h1>
+      <button className="btn">Button</button>
+    </div>
+  );
+};
+
+export default App;
